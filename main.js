7e940100deff02bd15b78ab4ee6a26396bfdb082
Ajay Desai <ajaydesai.pro@gmail.com>
2023-12-04 11:23:50 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+
+function hideDiv(){
+    for(i=1;i<=6;i++){
+        document.querySelector(".questions-p"+i).style.display = 'none';
+    }
+}
+hideDiv();
+
+function faqView(questionClass,btnClass){
+    let div = document.querySelector(questionClass);
+    let btn = document.querySelector(btnClass);
+
+    if(div.style.display == "block"){
+        btn.style.transform = "rotate(90deg)";
+        div.style.display = "none";
+    } else{
+        btn.style.transform = "rotate(45deg)";
+        div.style.display = "block";
+    }
+}
